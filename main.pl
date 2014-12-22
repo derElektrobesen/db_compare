0ff29202b2a6ab540616a08d7c8b4b7a5e473d6f
Pavel Berezhnoy <p.berezhnoy@corp.mail.ru>
2014-12-23 01:15:00 +0400

    Master process word implemented

--- main.pl
+++ main.pl
@@ -3,12 +3,48 @@
 use strict;
 use warnings;
 
+use Time::HiRes qw();
+
 my %params = (
     n_processes     => 5,
+    sleep_time      => 0.5, # in seconds
 );
 
 sub child_work {
     my $instance = shift;
+}
+
+sub master_work {
+    my %processes = map { $_{pid} => $_{name} } @{$_[0]}; # reference on array of references on hashes
+
+    open my $out_file, '>', "results_$$.log";
+
+    my $first_step = 1;
+    do {
+        Time::HiRes::sleep($params{sleep_time}) unless $first_step;
+        $first_step = 0;
+
+        my @processes_list_copy = keys %processes;
+        for (sort @processes_list_copy) {
+            unless (kill 0 => $_) {
+                # child process died
+                delete $processes{$_};
+            } else {
+                my $time = localtime;
+                my $content;
+
+                {
+                    local $/ = undef; # read all file at ones
+                    open my $stat_file, '<', "/proc/$_/stat";
+                    $content = <$stat_file>;
+                    close $stat_file;
+                }
+
+                chomp $content;
+                print $out_file "$time $processes{$_} $content\n";
+            }
+        }
+    } while (%processes);
 }
 
 sub create_instances {
@@ -17,19 +53,25 @@ sub create_instances {
 
 sub main {
     my $instance;
+    my @processes;
+
+    my $pid;
     for my $i (create_instances) {
         for (0 .. $params{n_processes}) {
-            unless (fork) {
+            $pid = fork;
+            unless ($pid) {
                 $instance = $i;
                 last;
             }
+
+            push @processes, { pid => $pid, name => $i->name };
         }
         last if defined $instance;
     }
 
-    if ($child) {
+    if ($pid) {
         # master process
-        while (wait > 0) {}
+        master_work \@processes;
     } else {
         # child process
         child_work $instance;
